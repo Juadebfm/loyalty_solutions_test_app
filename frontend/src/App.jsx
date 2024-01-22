a7e610c22d6b9aa47b086910f4ea9583baef1473
Julius Adebowale <juadebgabriel@gmail.com>
2024-01-22 08:58:50 +0100

    FEAT: Added a product provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+// import react router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+// import components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/context/ProductContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect } from "react";
+
+//Create Context
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  //products state
+  const [products, setProducts] = useState([]);
+  //fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://api.escuelajs.co/api/v1/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
